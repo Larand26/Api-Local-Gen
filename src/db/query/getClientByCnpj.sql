d79f4e095bd3baa60045e104a7b19df836e2bb4a
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-05-28 07:43:11 -0300

    refactor: adiciona o número de celular no retorno do cliente

--- src/db/query/getClientByCnpj.sql
+++ src/db/query/getClientByCnpj.sql
@@ -2,6 +2,8 @@ SELECT TOP(1)
 [ID_CODENTIDADE] AS client_id,
 [ENTI_RAZAOSOCIAL] AS corporate_name,
 [ENTI_CNPJCPF] as client_cnpj,
-[ID_CODVENDEDOR] AS salesperson_id
+[ID_CODVENDEDOR] AS salesperson_id,
+[ENTI_CELULAR] AS cellphone,
+[ENTI_DDD_CELULAR] AS cellphone_area_code
 FROM [ENTIDADES]
 WHERE [ENTI_CNPJCPF] = @cnpj
